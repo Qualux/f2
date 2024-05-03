d06865edfc6f16b4478cc34d423e5afa7dbebdf4
Casey Milne <casey@saberwp.com>
2024-05-03 11:17:34 +0100

    Draft filters field list.

--- apps/fields/src/components/fields/FieldList.js
+++ apps/fields/src/components/fields/FieldList.js
@@ -10,6 +10,7 @@ function EmptyMessage() {
 }
 
 function Field({field, index}) {
+
     return(
         <>
             <div className="font-medium text-xs text-zinc-800 px-2 py-1">
@@ -42,6 +43,30 @@ function Field({field, index}) {
                 </NavLink>
             </div>
         </>
+    );
+
+}
+
+function FieldListFilters() {
+    return(
+        <div className="my-8">
+            <h5 className="text-xs mb-6">
+                FILTER BY
+            </h5>
+            <div className="flex items-center justify-between">
+                <div>
+                    <h4>FIELD TYPE</h4>
+                    <div className="flex gap-1 items-center">
+                        <button>Text</button>
+                        <button>Button Group</button>
+                        <button>Number</button>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
@@ -54,21 +79,29 @@ export default function FieldList() {
     }
 
     return(
-        <div className="max-w-3xl grid grid-cols-4 gap-2">
-            <div className="font-bold text-sm text-neutral-800 px-2 py-1">
-                ID
+        <div className="max-w-3xl">
+            <FieldListFilters />
+            <div className="grid grid-cols-4 gap-2">
+                <div className="font-bold text-sm text-neutral-800 px-2 py-1">
+                    ID
+                </div>
+                <div className="font-bold text-sm text-neutral-800 px-2 py-1">
+                    Title
+                </div>
+                <div className="font-bold text-sm text-neutral-800 px-2 py-1">
+                    Type
+                </div>
+                <div>&nbsp;</div>
+                {fields.map( ( field, index ) =>
+                    <Field key={index} field={field} index={index} />
+                )}
             </div>
-            <div className="font-bold text-sm text-neutral-800 px-2 py-1">
-                Title
+            <div className="my-6 flex items-center justify-center">
+                <button className="inline-block bg-neutral-300 text-neutral-600 py-2 px-6">
+                    Load more (23 available)
+                </button>
             </div>
-            <div className="font-bold text-sm text-neutral-800 px-2 py-1">
-                Type
-            </div>
-            <div>&nbsp;</div>
-            {fields.map( ( field, index ) =>
-                <Field key={index} field={field} index={index} />
-            )}
         </div>
-    )
+    );
 
 }
