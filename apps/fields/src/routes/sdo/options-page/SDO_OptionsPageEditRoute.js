f108c6445ed39fd6fdf79ad7943f17efce91d25c
Casey Milne <casey@saberwp.com>
2024-05-17 22:48:07 +0300

    Options edit wrap.

--- apps/fields/src/routes/sdo/options-page/SDO_OptionsPageEditRoute.js
+++ apps/fields/src/routes/sdo/options-page/SDO_OptionsPageEditRoute.js
@@ -1,5 +1,6 @@
 import { useCrudible } from '../../../lib/useCrudible/useCrudible';
 import AppTemplate from '../../../components/global/AppTemplate';
+import ScreenWrap from '../../../components/global/ScreenWrap';
 
 export default function SDO_OptionsPageEditRoute() {
 
@@ -9,8 +10,10 @@ export default function SDO_OptionsPageEditRoute() {
 
     return(
         <AppTemplate>
-            <HeaderSDO />
-            <AppForm />
+            <HeaderSDO mode="edit" />
+            <ScreenWrap>
+                <AppForm />
+            </ScreenWrap> 
         </AppTemplate>
     );
 
