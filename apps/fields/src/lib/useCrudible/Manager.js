1a931b2b547c1872188d8301bd0fd8f7f5f12f9e
Casey Milne <casey@saberwp.com>
2024-05-22 00:08:59 +0300

    Updates to manager to useSDO.

--- apps/fields/src/lib/useCrudible/Manager.js
+++ apps/fields/src/lib/useCrudible/Manager.js
@@ -8,26 +8,21 @@ import ScreenWrap from '../../components/global/ScreenWrap';
 import SkeletonList from '../../components/global/SkeletonList';
 import { SDO_StandardAPI } from '../../api/SDO_StandardAPI';
 
-export default function Manager( {sdoKey, api} ) {
+export default function Manager() {
 
     const [page, setPage] = useState(1);
     const [sortColumn, setSortColumn] = useState('ID');
     const [sortOrder, setSortOrder] = useState('DESC');
     const [filterValues, setFilterValues] = useState(null);
+    const { HeaderSDO, Grid, Footer, useSDO } = useCrudible();
+    const sdo = useSDO();
 
-    
-    const { HeaderSDO, Grid, Footer, sdoRoutes, SDO_Context } = useCrudible();
-    
-    const sdo = useContext(SDO_Context);
-    console.log('sdo in Manager:')
+    console.log('sdo in manager:')
     console.log(sdo)
 
-
-    /* Newer SDO does not pass the API because it is uses the SDO_StandardAPI */
-    if( ! api ) {
-        api = SDO_StandardAPI;
-        api.routeBase = sdo.routeBase;
-    }
+    // Setup API.
+    const api = SDO_StandardAPI;
+    api.routeBase = sdo.routeBase;
 
     const {
         isLoading,
@@ -37,6 +32,9 @@ export default function Manager( {sdoKey, api} ) {
         queryFn: () => api.get(page, sortColumn, sortOrder, filterValues),
         placeholderData: keepPreviousData,
     });
+
+    console.log('data in manager:')
+    console.log(data)
 
     useEffect(() => {
         const initialFilterValues = Object.fromEntries(sdo.filters.map(filter => [filter.key, ''])); // Initialize all filters to empty string
@@ -54,12 +52,23 @@ export default function Manager( {sdoKey, api} ) {
         )
     }
 
+    if( data === undefined ) {
+        return(
+            <>
+                <HeaderSDO />
+                <ScreenWrap>
+                    <SkeletonList />
+                </ScreenWrap>
+            </>
+        );
+    }
+
     return(
         <>
             <HeaderSDO />
             <ScreenWrap>
                 <Grid 
-                    routes={sdoRoutes}
+                    routes={sdo.routes}
                     data={data} 
                     columns={sdo.columns}
                     page={page}
