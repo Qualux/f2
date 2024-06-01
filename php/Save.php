e8e779d2764059c5295a04c9b3993a1419e9ef3a
Casey Milne <casey@saberwp.com>
2024-06-01 04:02:26 +0300

    Save API route.

--- php/Save.php
+++ php/Save.php
@@ -30,14 +30,24 @@ class Save {
                 'methods' => 'POST',
                 'callback' => function( \WP_REST_Request $req ) {
 
-                    $params = $req->get_json_params();
-                    $form   = $params['form'];
+                    $params   = $req->get_json_params();
+                    
+                    $location = $params['location'];
+                    $record   = $params['record'];
+                    $form     = $params['form'];
+                    $values   = $params['values'];
+
+                    $result = $this->process( $location, $record, $form, $values );
 
                     return new \WP_REST_Response(
                         array(
                             'status'   => 200,
                             'message'  => __( 'Saved.', 'f3' ),
+                            'location' => $location,
+                            'record'   => $record,
                             'form'     => $form,
+                            'values'   => $values,
+                            'result'   => $result,
                         )
                     );
 
@@ -48,6 +58,50 @@ class Save {
                 },
             )
         );
+
+    }
+
+    function process( $location, $record, $form, $values ) {
+
+        $result = '';
+
+        // Pair the fields and their values.
+
+        $field_values = array();
+        foreach( $form['field_groups'] as $field_group ) {
+            foreach( $field_group['fields'] as $field ) {
+                $field_value = array(
+                    'field' => $field['name'],
+                    'value' => $values[ $field['name'] ],
+                );
+                $field_values[] = $field_value;
+            }
+        }
+
+        foreach( $field_values as $field_value ) {
+
+            switch( $location ) {
+                case 'post':
+                    update_post_meta( $record, $field_value['field'], $field_value['value'] );
+                    $result .= 'Stored post meta key ' . $field_value['field'] . '.';
+                    break;
+                case 'term':
+                    update_term_meta( $record, $field_value['field'], $field_value['value'] );
+                    $result .= 'Stored term meta key ' . $field_value['field'] . '.';
+                    break;
+                case 'user':
+                    update_user_meta( $record, $field_value['field'], $field_value['value'] );
+                    $result .= 'Stored user meta key ' . $field_value['field'] . '.';
+                    break;
+                case 'option':
+                    update_option( $field_value['field'], $field_value['value'] );
+                    $result .= 'Stored option key ' . $field_value['field'] . '.';
+                    break;
+            }
+
+        }
+
+        return $result;
 
     }
 
